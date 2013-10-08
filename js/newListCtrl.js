b64282582b4cd1785af7278f2ae9dea1552c9b25
harokb <harokb@gmail.com>
2013-10-08 22:02:29 +0100

    Renamed file from listCtrl.js

--- js/newListCtrl.js
+++ js/newListCtrl.js
@@ -1,14 +1,18 @@
-function listCtrl($scope) {
+function newListCtrl($scope) {
 
     var LIST_NAMES_KEY = 'all.list.names';
     var CURRENT_LIST_NAME_KEY = 'list.current_list_name';
     $scope.listNames = getFromLocalStorage(LIST_NAMES_KEY) || ['Default'];
 
     $scope.addList = function (listName) {
-        $scope.listNames.push(listName);
-        saveToLocalStorage(LIST_NAMES_KEY, $scope.listNames);
+        if ($scope.listNames.indexOf(listName) !== -1) {
+            $scope.listNames.push(listName);
+            saveToLocalStorage(LIST_NAMES_KEY, $scope.listNames);
+        }
         saveToLocalStorage(CURRENT_LIST_NAME_KEY, listName);
         window.location.replace('index.html');
     }
+
+
 
 }
